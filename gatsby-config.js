efdc45f68670e871f1d459ba965d964a64de03ae
Eric Phifer <ericoncode@gmail.com>
2022-05-17 07:16:55 -0600

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+ require('dotenv').config({
+  path: '.env',
+});
 
 module.exports = {
+  siteMetadata: {
+    title: 'Active Insurance Solutions',
+    siteUrl: 'https://activeinsurancegj.com',
+    description: 'Good Health. That\'s the plan.',
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        apiVersion: '2022-05-17',
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sanity-image',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+      },
+    },
+  ],
 }
